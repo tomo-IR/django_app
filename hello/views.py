89b0ff43cf2936df90c2fffc62efd1d2224c3b32
tomo <sunnyday20200101@gmail.com>
2023-11-30 09:39:06 +0900

    add form class

--- hello/views.py
+++ hello/views.py
@@ -1,17 +1,33 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .forms import HelloForm
 
 def index(request):
     params = {
-            'title':'Hello/Index',
-            'msg':'お名前は？',
+            'title': 'Hello',
+            'message': 'your data:',
+            'form': HelloForm()
         }
+    if (request.method == 'POST'):
+        params['message'] = '名前：' + request.POST['name'] + \
+            '<br>メール：' + request.POST['mail'] + \
+            '<br>年齢：' + request.POST['age']
+        return render(request, 'hello/index.html', params)
+
+from django.shortcuts import render
+from django.http import HttpResponse
+from .forms import HelloForm
+
+def index(request):
+    params = {
+            'title': 'Hello',
+            'message': 'your data:',
+            'form': HelloForm()
+        }
+    if (request.method == 'POST'):
+        params['message'] = '名前：' + request.POST['name'] + \
+            '<br>メール：' + request.POST['mail'] + \
+            '<br>年齢：' + request.POST['age']
+        params['form'] = HelloForm(request.POST)
     return render(request, 'hello/index.html', params)
 
-def form(request):
-    msg = request.POST['msg']
-    params = {
-            'title':'Hello/Form',
-            'msg':'こんにちは、' + msg + 'さん。',
-        }
-    return render(request, 'hello/index.html', params)
