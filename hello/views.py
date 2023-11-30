e5c36c7d221c3523f84d2398eae25651d6367971
tomo <sunnyday20200101@gmail.com>
2023-11-30 09:25:23 +0900

    add form

--- hello/views.py
+++ hello/views.py
@@ -1,23 +1,17 @@
-
-
-# Create your views here.
 from django.shortcuts import render
 from django.http import HttpResponse
-
-
 
 def index(request):
     params = {
             'title':'Hello/Index',
-            'msg':'これは、サンプルで作ったページです。',
-            'goto':'next',
+            'msg':'お名前は？',
         }
     return render(request, 'hello/index.html', params)
 
-def next(request):
+def form(request):
+    msg = request.POST['msg']
     params = {
-            'title':'Hello/Next',
-            'msg':'これは、もう１つのページです。',
-            'goto':'index',
+            'title':'Hello/Form',
+            'msg':'こんにちは、' + msg + 'さん。',
         }
     return render(request, 'hello/index.html', params)
