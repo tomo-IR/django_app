bc9f96790deb8edd9550e187b1c3f1fbe6b83fa1
tomo <sunnyday20200101@gmail.com>
2023-12-05 22:55:10 +0900

    POST

--- hello/forms.py
+++ hello/forms.py
@@ -1,7 +1,4 @@
 from django import forms
 
 class HelloForm(forms.Form):
-    # name = forms.CharField(label='name')
-    # mail = forms.CharField(label='mail')
-    # age = forms.IntegerField(label='age')
-    check = forms.NullBooleanField(label='Check')
+    id = forms.IntegerField(label='ID')
--- hello/views.py
+++ hello/views.py
@@ -1,23 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.views.generic import TemplateView
-from .forms import HelloForm
 
-class HelloView(TemplateView):
-    
-    def __init__(self):
-        self.params = {
-                'title': 'Hello',
-                'form': HelloForm(),
-                'result': None
-            }
-    
-    def get(self, request):
-        return render(request, 'hello/index.html', self.params)
+from hello.forms import HelloForm
+from .models import Friend
 
-    def post(self, request):
-        chk = request.POST['check']
-        self.params['result'] = 'you selected: "' + chk + '".'
-        self.params['form'] = HelloForm(request.POST)
-        return render(request, 'hello/index.html', self.params)
-
+def index(request):
+    # data = Friend.objects.all()
+    params = {
+            'title': 'Hello',
+            'message': 'all friends.',
+            'form': HelloForm(),
+            'data': [],
+        }
+    if (request.method == 'POST'):
+        num=request.POST['id']
+        item = Friend.objects.get(id=num)
+        params['data'] = [item]
+        params['form'] = HelloForm(request.POST)
+    else:
+        params['data'] = Friend.objects.all()
+    return render(request, 'hello/index.html', params)
