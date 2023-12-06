71a4a0b5d72f3195d45ed4817beac482b80e208d
tomo <sunnyday20200101@gmail.com>
2023-12-06 22:58:09 +0900

    query set

--- hello/views.py
+++ hello/views.py
@@ -3,20 +3,22 @@ from django.http import HttpResponse
 
 from hello.forms import HelloForm
 from .models import Friend
+from django.db.models import QuerySet
+def __new_str__(self):
+    result = ''
+    for item in self:
+        result += '<tr>'
+        for k in item:
+            result += '<td>' + str(k) + '=' + str(item[k]) + '</td>'
+        result += '</tr>'
+    return result
+
+QuerySet.__str__ = __new_str__
 
 def index(request):
-    # data = Friend.objects.all()
+    data = Friend.objects.all().values('id', 'name', 'age')
     params = {
             'title': 'Hello',
-            'message': 'all friends.',
-            'form': HelloForm(),
-            'data': [],
+            'data': data,
         }
-    if (request.method == 'POST'):
-        num=request.POST['id']
-        item = Friend.objects.get(id=num)
-        params['data'] = [item]
-        params['form'] = HelloForm(request.POST)
-    else:
-        params['data'] = Friend.objects.all()
     return render(request, 'hello/index.html', params)
