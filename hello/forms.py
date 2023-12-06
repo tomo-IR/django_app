0b31dea7b647f28377e194e182fec4d673058e8c
tomo <sunnyday20200101@gmail.com>
2023-12-06 23:31:45 +0900

    CREATE

--- hello/forms.py
+++ hello/forms.py
@@ -1,4 +1,7 @@
 from django import forms
+from.models import Friend
 
-class HelloForm(forms.Form):
-    id = forms.IntegerField(label='ID')
+class FriendForm(forms.ModelForm):
+    class Meta:
+        model = Friend
+        fields = ['name','mail','gender','age','birthday']
--- hello/views.py
+++ hello/views.py
@@ -1,24 +1,30 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
-from hello.forms import HelloForm
+from django.shortcuts import redirect
 from .models import Friend
-from django.db.models import QuerySet
-def __new_str__(self):
-    result = ''
-    for item in self:
-        result += '<tr>'
-        for k in item:
-            result += '<td>' + str(k) + '=' + str(item[k]) + '</td>'
-        result += '</tr>'
-    return result
-
-QuerySet.__str__ = __new_str__
+from .forms import FriendForm
 
 def index(request):
-    data = Friend.objects.all().values('id', 'name', 'age')
+    data = Friend.objects.all()
     params = {
             'title': 'Hello',
             'data': data,
         }
     return render(request, 'hello/index.html', params)
+
+# create model
+
+def create(request):
+    if (request.method == 'POST'):
+        obj = Friend()
+        friend = FriendForm(request.POST, instance=obj)
+        friend.save()
+        return redirect(to='/hello')
+    params = {
+        'title': 'Hello',
+        'form': FriendForm(),
+    }
+    return render(request, 'hello/create.html', params)
+
+
+
